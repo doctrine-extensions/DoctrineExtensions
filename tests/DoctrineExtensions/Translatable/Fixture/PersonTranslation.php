3a5ea89c8e718327968a06977a2544e281ac0845
gediminasm <gediminas.morkevicius@gmail.com>
2010-11-11 22:56:21 +0200

    translation entity mapped supperclass

--- tests/DoctrineExtensions/Translatable/Fixture/PersonTranslation.php
+++ tests/DoctrineExtensions/Translatable/Fixture/PersonTranslation.php
@@ -2,55 +2,15 @@
 
 namespace Translatable\Fixture;
 
+use DoctrineExtensions\Translatable\Entity\TranslationTemplate;
+
 /**
  * @Table(name="person_translations", indexes={
  *      @index(name="person_translation_idx", columns={"locale", "entity", "foreign_key", "field"})
  * })
  * @Entity(repositoryClass="DoctrineExtensions\Translatable\Repository\TranslationRepository")
  */
-class PersonTranslation
+class PersonTranslation extends TranslationTemplate
 {
-    /**
-     * @var integer $id
-     *
-     * @Column(name="id", type="integer")
-     * @Id
-     * @GeneratedValue(strategy="IDENTITY")
-     */
-    private $id;
-
-    /**
-     * @var string $locale
-     *
-     * @Column(name="locale", type="string", length=8)
-     */
-    private $locale;
-
-    /**
-     * @var string $entity
-     *
-     * @Column(name="entity", type="string", length=255)
-     */
-    private $entity;
-
-    /**
-     * @var string $field
-     *
-     * @Column(name="field", type="string", length=32)
-     */
-    private $field;
-
-    /**
-     * @var string $foreignKey
-     *
-     * @Column(name="foreign_key", type="string", length="64")
-     */
-    private $foreignKey;
-
-    /**
-     * @var text $content
-     *
-     * @Column(name="content", type="text", nullable=true)
-     */
-    private $content;
+    
 }
