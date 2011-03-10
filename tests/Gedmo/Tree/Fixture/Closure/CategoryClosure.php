b11fee17058d9f2850878d24cffd41e333a101c1
Gustavo Adrian <comfortablynumb84@gmail.com>
2011-03-10 12:12:26 -0300

    [Tree - Closure Table] Added ClosureTreeRepository and unit tests

--- tests/Gedmo/Tree/Fixture/Closure/CategoryClosure.php
+++ tests/Gedmo/Tree/Fixture/Closure/CategoryClosure.php
@@ -5,7 +5,7 @@ namespace Tree\Fixture\Closure;
 use Gedmo\Tree\Entity\AbstractClosure;
 
 /**
- * @Entity
+ * @Entity(repositoryClass="Gedmo\Tree\Entity\Repository\ClosureTreeRepository")
  */
 class CategoryClosure extends AbstractClosure
 {
