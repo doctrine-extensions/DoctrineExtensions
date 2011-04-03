502003f57d3e7aa7ce2639a7caba3aa19eee5b55
gediminasm <gediminas.morkevicius@gmail.com>
2011-04-03 14:24:51 +0200

    [mapping] event argument adapter for single listener support

--- lib/Gedmo/Mapping/MappedEventSubscriber.php
+++ lib/Gedmo/Mapping/MappedEventSubscriber.php
@@ -12,11 +12,11 @@ use Gedmo\Mapping\ExtensionMetadataFactory,
  * This is extension of event subscriber class and is
  * used specifically for handling the extension metadata
  * mapping for extensions.
- * 
+ *
  * It dries up some reusable code which is common for
  * all extensions who mapps additional metadata through
  * extended drivers
- * 
+ *
  * @author Gediminas Morkevicius <gediminas.morkevicius@gmail.com>
  * @package Gedmo.Mapping
  * @subpackage MappedEventSubscriber
@@ -24,26 +24,59 @@ use Gedmo\Mapping\ExtensionMetadataFactory,
  * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 abstract class MappedEventSubscriber implements EventSubscriber
-{    
+{
     /**
      * List of cached object configurations
-     *  
+     *
      * @var array
      */
     protected $configurations = array();
-    
+
     /**
      * ExtensionMetadataFactory used to read the extension
      * metadata through the extension drivers
-     * 
+     *
      * @var Gedmo\Mapping\ExtensionMetadataFactory
      */
-    protected $extensionMetadataFactory = null;
-    
+    private $extensionMetadataFactory;
+
+    /**
+     * List of event adapters used for this listener
+     *
+     * @var array
+     */
+    private $adapters = array();
+
+    /**
+     * Get an event adapter to handle event specific
+     * methods
+     *
+     * @param EventArgs $args
+     * @throws \Gedmo\Exception\InvalidArgumentException - if event is not recognized
+     * @return \Gedmo\Mapping\Event\AdapterInterface
+     */
+    protected function getEventAdapter(EventArgs $args)
+    {
+        $class = get_class($args);
+        if (preg_match('@Doctrine\\\([^\\\]+)@', $class, $m) && in_array($m[1], array('ODM', 'ORM'))) {
+            if (!isset($this->adapters[$m[1]])) {
+                $adapterClass = $this->getNamespace() . '\\Mapping\\Event\\Adapter\\' . $m[1];
+                if (!class_exists($adapterClass)) {
+                    $adapterClass = 'Gedmo\\Mapping\\Event\\Adapter\\'.$m[1];
+                }
+                $this->adapters[$m[1]] = new $adapterClass;
+            }
+            $this->adapters[$m[1]]->setEventArgs($args);
+            return $this->adapters[$m[1]];
+        } else {
+            throw new \Gedmo\Exception\InvalidArgumentException('Event mapper does not support event arg class: '.$class);
+        }
+    }
+
     /**
      * Get the configuration for specific object class
      * if cache driver is present it scans it also
-     * 
+     *
      * @param ObjectManager $objectManager
      * @param string $class
      * @return array
@@ -62,10 +95,10 @@ abstract class MappedEventSubscriber implements EventSubscriber
         }
         return $config;
     }
-    
+
     /**
      * Get extended metadata mapping reader
-     * 
+     *
      * @param ObjectManager $objectManager
      * @return Gedmo\Mapping\ExtensionMetadataFactory
      */
@@ -79,11 +112,11 @@ abstract class MappedEventSubscriber implements EventSubscriber
         }
         return $this->extensionMetadataFactory;
     }
-    
+
     /**
      * Scans the objects for extended annotations
      * event subscribers must subscribe to loadClassMetadata event
-     * 
+     *
      * @param ObjectManager $objectManager
      * @param ClassMetadata $metadata
      * @return void
@@ -96,11 +129,12 @@ abstract class MappedEventSubscriber implements EventSubscriber
             $this->configurations[$metadata->name] = $config;
         }
     }
-    
+
     /**
      * Get the namespace of extension event subscriber.
-     * used for cache id of extensions
-     * 
+     * used for cache id of extensions also to know where
+     * to find Mapping drivers and event adapters
+     *
      * @return string
      */
     abstract protected function getNamespace();
