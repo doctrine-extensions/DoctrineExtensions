14bd03ee205b10b608ca3a9f04e4361d49440027
Gediminas Morkevicius <gediminas.morkevicius@knplabs.com>
2011-04-27 11:21:21 +0200

    [mapping] applied fix from @jaugustin, referencing PR #41, closes #40

--- lib/Gedmo/Mapping/MappedEventSubscriber.php
+++ lib/Gedmo/Mapping/MappedEventSubscriber.php
@@ -36,7 +36,7 @@ abstract class MappedEventSubscriber implements EventSubscriber
      *
      * @var Gedmo\Mapping\ExtensionMetadataFactory
      */
-    private $extensionMetadataFactory;
+    private $extensionMetadataFactory = array();
 
     /**
      * List of event adapters used for this listener
@@ -102,13 +102,14 @@ abstract class MappedEventSubscriber implements EventSubscriber
      */
     public function getExtensionMetadataFactory($objectManager)
     {
-        if (null === $this->extensionMetadataFactory) {
-            $this->extensionMetadataFactory = new ExtensionMetadataFactory(
+        $class = get_class($objectManager);
+        if (!isset($this->extensionMetadataFactory[$class])) {
+            $this->extensionMetadataFactory[$class] = new ExtensionMetadataFactory(
                 $objectManager,
                 $this->getNamespace()
             );
         }
-        return $this->extensionMetadataFactory;
+        return $this->extensionMetadataFactory[$class];
     }
 
     /**
