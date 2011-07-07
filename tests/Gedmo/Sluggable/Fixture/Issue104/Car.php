0ee342be308dc3a3aa1a5e7e5ae37957da9ea538
gediminasm <gediminas.morkevicius@gmail.com>
2011-07-07 20:29:17 +0200

    [tests] tests for issue104, closes #104

---
+++ tests/Gedmo/Sluggable/Fixture/Issue104/Car.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Sluggable\Fixture\Issue104;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * @ORM\Entity
+ */
+class Car extends Vehicle
+{
+    /**
+     * @Gedmo\Sluggable
+     * @ORM\Column(length=128)
+     */
+    protected $title;
+
+    /**
+     * @ORM\Column(length=128)
+     */
+    private $description;
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
