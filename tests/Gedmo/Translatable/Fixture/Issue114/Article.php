ba9c8bf74e09fec1b2ad538461bd7a4f95507c7b
“Oscar <ing.oscar.balladares@gmail.com>
2011-07-17 16:39:36 -0600

    Added Fixtures and testcase for Issue 114

---
+++ tests/Gedmo/Translatable/Fixture/Issue114/Article.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Translatable\Fixture\Issue114;
+
+use Gedmo\Mapping\Annotation as Gedmo;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+class Article
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @Gedmo\Translatable
+     * @ORM\Column(name="title", type="string", length=128)
+     */
+    private $title;
+    
+    /**
+     * @ORM\ManyToOne(targetEntity="Category", inversedBy="articles")
+     */
+    private $category;
+
+    
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+    
+    public function setCategory(Category $category)
+    {
+        $this->category = $category;
+    }
+    
+    public function getCategory()
+    {
+        return $this->category;
+    }
+}
+
