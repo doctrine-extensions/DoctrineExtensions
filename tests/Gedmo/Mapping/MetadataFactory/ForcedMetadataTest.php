ff51d6bd3d57938027fd16059de4f8e573faa470
gediminasm <gediminas.morkevicius@gmail.com>
2011-11-01 23:52:59 +0200

    [mapping] more metadata factory tests, references #134

--- tests/Gedmo/Mapping/MetadataFactory/ForcedMetadataTest.php
+++ tests/Gedmo/Mapping/MetadataFactory/ForcedMetadataTest.php
@@ -1,7 +1,6 @@
 <?php
 
-use Doctrine\ORM\Mapping\ClassMetadataInfo;
-use Doctrine\ORM\Mapping\Driver\Driver;
+use Doctrine\ORM\Mapping\ClassMetadata;
 use Mapping\Fixture\Unmapped\Timestampable;
 
 /**
@@ -12,14 +11,16 @@ use Mapping\Fixture\Unmapped\Timestampable;
 * @link http://www.gediminasm.org
 * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
 */
-class FactoryMetadataTest extends \PHPUnit_Framework_TestCase
+class ForcedMetadataTest extends \PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
         $config = new \Doctrine\ORM\Configuration();
         $config->setProxyDir(TESTS_TEMP_DIR);
         $config->setProxyNamespace('Gedmo\Mapping\Proxy');
-        $config->setMetadataDriverImpl(new CustomDriver);
+        $config->setMetadataDriverImpl(
+            new \Doctrine\ORM\Mapping\Driver\AnnotationDriver($_ENV['annotation_reader'])
+        );
 
         $conn = array(
             'driver' => 'pdo_sqlite',
@@ -31,6 +32,36 @@ class FactoryMetadataTest extends \PHPUnit_Framework_TestCase
         $this->timestampable->setAnnotationReader($_ENV['annotation_reader']);
         $evm->addEventSubscriber($this->timestampable);
         $this->em = \Doctrine\ORM\EntityManager::create($conn, $config, $evm);
+    }
+
+    private function prepare()
+    {
+        $cmf = $this->em->getMetadataFactory();
+        $metadata = new ClassMetadata('Mapping\Fixture\Unmapped\Timestampable');
+        $id = array();
+        $id['fieldName'] = 'id';
+        $id['type'] = 'integer';
+        $id['nullable'] = false;
+        $id['columnName'] = 'id';
+        $id['id'] = true;
+
+        $metadata->mapField($id);
+
+        $created = array();
+        $created['fieldName'] = 'created';
+        $created['type'] = 'datetime';
+        $created['nullable'] = false;
+        $created['columnName'] = 'created';
+
+        $metadata->mapField($created);
+        $metadata->setIdGeneratorType(ClassMetadata::GENERATOR_TYPE_IDENTITY);
+        $metadata->setIdGenerator(new \Doctrine\ORM\Id\IdentityGenerator(null));
+        $cmf->setMetadataFor('Mapping\Fixture\Unmapped\Timestampable', $metadata);
+
+        // trigger loadClassMetadata event
+        $evm = $this->em->getEventManager();
+        $eventArgs = new \Doctrine\ORM\Event\LoadClassMetadataEventArgs($metadata, $this->em);
+        $evm->dispatchEvent(\Doctrine\ORM\Events::loadClassMetadata, $eventArgs);
 
         $schemaTool = new \Doctrine\ORM\Tools\SchemaTool($this->em);
         $schemaTool->dropSchema(array());
@@ -44,6 +75,9 @@ class FactoryMetadataTest extends \PHPUnit_Framework_TestCase
      */
     public function shouldWork()
     {
+        $this->prepare();
+
+        $meta = $this->em->getClassMetadata('Mapping\Fixture\Unmapped\Timestampable');
         // driver falls back to annotation driver
         $conf = $this->timestampable->getConfiguration(
             $this->em,
@@ -61,44 +95,5 @@ class FactoryMetadataTest extends \PHPUnit_Framework_TestCase
             ->getValue($test)
         ;
         $this->assertFalse(empty($id));
-    }
-}
-
-class CustomDriver implements Driver
-{
-    public function getAllClassNames()
-    {
-        return array('Mapping\Fixture\Unmapped\Timestampable');
-    }
-
-    public function loadMetadataForClass($className, ClassMetadataInfo $metadata)
-    {
-        if ($className === 'Mapping\Fixture\Unmapped\Timestampable') {
-            $id = array();
-            $id['fieldName'] = 'id';
-            $id['type'] = 'integer';
-            $id['nullable'] = false;
-            $id['columnName'] = 'id';
-            $id['id'] = true;
-
-            $metadata->setIdGeneratorType(
-                constant('Doctrine\ORM\Mapping\ClassMetadata::GENERATOR_TYPE_AUTO')
-            );
-
-            $metadata->mapField($id);
-
-            $created = array();
-            $created['fieldName'] = 'created';
-            $created['type'] = 'datetime';
-            $created['nullable'] = false;
-            $created['columnName'] = 'created';
-
-            $metadata->mapField($created);
-        }
-    }
-
-    public function isTransient($className)
-    {
-        return !in_array($className, $this->getAllClassNames());
     }
 }
