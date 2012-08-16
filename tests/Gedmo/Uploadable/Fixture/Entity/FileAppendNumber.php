f3013b4b499122805676e0daa5c5e36289105ca1
comfortablynumb <comfortablynumb84@gmail.com>
2012-08-15 23:58:53 -0300

    [Uploadable] Modified fixtures in tests so we cover more use cases.

--- tests/Gedmo/Uploadable/Fixture/Entity/FileAppendNumber.php
+++ tests/Gedmo/Uploadable/Fixture/Entity/FileAppendNumber.php
@@ -25,7 +25,7 @@ class FileAppendNumber
     private $title;
 
     /**
-     * @ORM\Column(name="path", type="string", nullable=true)
+     * @ORM\Column(name="path", type="string")
      * @Gedmo\UploadableFilePath
      */
     private $filePath;
