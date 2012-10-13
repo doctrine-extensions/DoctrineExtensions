31869cfc03fc5797f187f2eceee85558393f6ee7
Christoph Kraemer <mail@christophkraemer.de>
2012-10-13 11:48:05 +0200

    Added additional slug handler tests

--- tests/Gedmo/Sluggable/Handlers/TreeSlugHandlerTest.php
+++ tests/Gedmo/Sluggable/Handlers/TreeSlugHandlerTest.php
@@ -46,6 +46,15 @@ class TreeSlugHandlerTest extends BaseTestCaseORM
 
         $citrons = $repo->findOneByTitle('Citrons');
         $this->assertEquals('food/fruits/citrons', $citrons->getSlug());
+
+        $apple = $repo->findOneByTitle('Apple');
+        $this->assertEquals('food/fruits/apple', $apple->getSlug());
+
+        //$kiwi = $repo->findOneByTitle('Kiwi');
+        //$this->assertEquals('food/fruits/kiwi', $kiwi->getSlug());
+
+        //$banana = $repo->findOneByTitle('Bananan');
+        //$this->assertEquals('food/fruits/banana', $banana->getSlug());
     }
 
     public function testSlugUpdates()
@@ -146,6 +155,15 @@ class TreeSlugHandlerTest extends BaseTestCaseORM
         $citrons = new TreeSlug;
         $citrons->setTitle('Citrons');
 
+        $apple = new TreeSlug;
+        $apple->setTitle('Apple');
+
+        $kiwi = new TreeSlug;
+        $kiwi->setTitle('Kiwi');
+
+        $banana = new TreeSlug;
+        $banana->setTitle('Banana');
+
         $repo
             ->persistAsFirstChild($food)
             ->persistAsFirstChildOf($fruits, $food)
@@ -153,7 +171,10 @@ class TreeSlugHandlerTest extends BaseTestCaseORM
             ->persistAsLastChildOf($milk, $food)
             ->persistAsLastChildOf($meat, $food)
             ->persistAsFirstChildOf($oranges, $fruits)
-            ->persistAsFirstChildOf($citrons, $fruits);
+            ->persistAsFirstChildOf($citrons, $fruits)
+            ->persistAsFirstChildOf($apple, $fruits)
+            ->persistAsPrevSiblingOf($kiwi, $apple)
+            ->persistAsNextSiblingOf($banana, $apple);
 
         $this->em->flush();
     }
