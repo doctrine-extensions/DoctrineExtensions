76e688975c7d7b563b9dbbac347b6ce055d7ee19
jandevani <jan@devani.nl>
2012-11-16 09:08:09 +0100

    Update lib/Gedmo/Uploadable/UploadableListener.php
    Fixed removal of /

--- lib/Gedmo/Uploadable/UploadableListener.php
+++ lib/Gedmo/Uploadable/UploadableListener.php
@@ -301,7 +301,7 @@ class UploadableListener extends MappedEventSubscriber
 
         Validator::validatePath($path);
 
-        $path = substr($path, strlen($path) - 1) === DIRECTORY_SEPARATOR ? substr($path, 0, strlen($path) - 2) : $path;
+        $path = rtrim($path, DIRECTORY_SEPARATOR);
 
         if ($config['fileMimeTypeField']) {
             $fileMimeTypeField = $refl->getProperty($config['fileMimeTypeField']);
