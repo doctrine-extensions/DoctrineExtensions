1f94f656b1db2d6a2a086c8e2a1ad81109c7dc93
Matthieu Napoli <matthieu@mnapoli.fr>
2013-04-17 17:57:35 +0200

    Reverting previous commit

--- tests/Gedmo/References/ReferencesListenerTest.php
+++ tests/Gedmo/References/ReferencesListenerTest.php
@@ -19,10 +19,6 @@ class ReferencesListenerTest extends BaseTestCaseOM
     {
         parent::setUp();
 
-        if (!class_exists('Mongo')) {
-            $this->markTestSkipped('Missing Mongo extension.');
-        }
-
         $reader = new AnnotationReader();
 
         $this->dm = $this->getMockDocumentManager('test', new MongoDBAnnotationDriver($reader, __DIR__ . '/Fixture/ODM/MongoDB'));
