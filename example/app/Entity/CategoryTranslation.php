c2b964246a0a78fe474d803d740e23d6fb90c194
gedi <gediminas.morkevicius@gmail.com>
2013-07-07 11:51:48 +0300

    [example] update based on changes

--- example/app/Entity/CategoryTranslation.php
+++ example/app/Entity/CategoryTranslation.php
@@ -3,35 +3,84 @@
 namespace Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Gedmo\Translatable\Entity\MappedSuperclass\AbstractPersonalTranslation;
+use Gedmo\Translatable\Entity\MappedSuperclass\AbstractTranslation;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * @ORM\Entity
- * @ORM\Table(name="category_translations",
- *     uniqueConstraints={@ORM\UniqueConstraint(name="lookup_unique_idx", columns={
- *         "locale", "object_id", "field"
- *     })}
- * )
+ * @ORM\Table(name="category_translations", uniqueConstraints={
+ *   @ORM\UniqueConstraint(name="lookup_unique_idx", columns={"locale", "object_id"}),
+ *   @ORM\UniqueConstraint(name="slug_unique_idx", columns={"slug"})
+ * })
  */
-class CategoryTranslation extends AbstractPersonalTranslation
+class CategoryTranslation extends AbstractTranslation
 {
+    /**
+     * @ORM\ManyToOne(targetEntity="Category", inversedBy="translations")
+     * @ORM\JoinColumn(name="object_id", referencedColumnName="id", onDelete="CASCADE", nullable=false)
+     */
+    protected $object;
+
+    /**
+     * @ORM\Column(length=64)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @Gedmo\Slug(fields={"title"})
+     * @ORM\Column(length=64, unique=true)
+     */
+    private $slug;
+
     /**
      * Convinient constructor
      *
      * @param string $locale
-     * @param string $field
-     * @param string $value
+     * @param string $title
+     * @param string $description
      */
-    public function __construct($locale, $field, $value)
+    public function __construct($locale = null, $title = null, $description = null)
     {
-        $this->setLocale($locale);
-        $this->setField($field);
-        $this->setContent($value);
+        $this->locale = $locale;
+        $this->title = $title;
+        $this->description = $description;
     }
 
-    /**
-     * @ORM\ManyToOne(targetEntity="Category", inversedBy="translations")
-     * @ORM\JoinColumn(name="object_id", referencedColumnName="id", onDelete="CASCADE")
-     */
-    protected $object;
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+        return $this;
+    }
+
+    public function getSlug()
+    {
+        return $this->slug;
+    }
 }
