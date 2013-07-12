2759c05df971ef9bafa5b69817847d9432ddd772
gedi <gediminas.morkevicius@gmail.com>
2013-07-12 21:58:51 +0300

    [translatable] handle slug and changeset difference

--- tests/Translatable/UniqueSlugTranslationTest.php
+++ tests/Translatable/UniqueSlugTranslationTest.php
@@ -27,7 +27,7 @@ class UniqueSlugTranslationTest extends BaseTestCaseORM
     /**
      * @test
      */
-    function shouldTranslateTranslatableEntityFields()
+    function shouldTranslateTranslatableEntityFieldsByPersistingTranslation()
     {
         // test new
         $food = new Post;
@@ -59,6 +59,57 @@ class UniqueSlugTranslationTest extends BaseTestCaseORM
 
         $post = $this->em->getRepository('Fixture\Translatable\Sluggable\Post')->findOneById($next->getId());
         $this->assertSame('next-post', $post->getSlug());
+    }
+
+    /**
+     * @test
+     */
+    function shouldTranslateTranslatableEntityFieldsNaturally()
+    {
+        // test new
+        $food = new Post;
+        $food->setTitle('Food'); // en translation through default
+        $this->em->persist($food);
+
+        $next = new Post;
+        $next->setTitle('Carrot');
+        $this->em->persist($next);
+
+        $this->em->flush();
+        $this->em->flush(); // should not trigger any change
+
+        $this->assertTranslationSlugIs($food->getTranslations(), 'en', 'food');
+        $this->assertTranslationSlugIs($next->getTranslations(), 'en', 'carrot');
+
+        // the original field should have slug as well
+        $this->assertSame('food', $food->getSlug());
+        $this->assertSame('carrot', $next->getSlug());
+
+        // now different language
+        $this->translatable->setTranslatableLocale('de');
+        $food->setTitle('Carrot');
+        $this->em->persist($food);
+
+        $next->setTitle('Food');
+        $this->em->persist($next);
+        $this->em->flush();
+
+        // the original field should have slug as well
+        $this->assertSame('carrot-1', $food->getSlug());
+        $this->assertSame('food-1', $next->getSlug());
+
+        $this->assertTranslationSlugIs($food->getTranslations(), 'de', 'carrot-1');
+        $this->assertTranslationSlugIs($next->getTranslations(), 'de', 'food-1');
+
+        $this->em->clear();
+        // fetch back in english
+        $this->translatable->setTranslatableLocale('en');
+
+        $food = $this->em->getRepository('Fixture\Translatable\Sluggable\Post')->findOneById($food->getId());
+        $this->assertSame('food', $food->getSlug());
+
+        $next = $this->em->getRepository('Fixture\Translatable\Sluggable\Post')->findOneById($next->getId());
+        $this->assertSame('carrot', $next->getSlug());
     }
 
     private function assertTranslationSlugIs(PersistentCollection $translations, $locale, $slug)
