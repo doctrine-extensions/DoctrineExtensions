aa0265321357f40a35a1639cef9bbfd52331ef98
Damien Matabon <damien.matabon@gmail.com>
2014-11-14 21:54:43 -0400

    Fix MongoDB Sortable when we set SortableGroup on a reference document

---
+++ tests/Gedmo/Sortable/Fixture/Document/Kid.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Sortable\Fixture\Document;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * @ODM\Document(collection="kids")
+ */
+class Kid
+{
+    /** @ODM\Id */
+    private $id;
+
+    /**
+     * @ODM\String
+     */
+    private $lastname;
+
+    /**
+     * @Gedmo\SortablePosition
+     * @ODM\Field(type="int")
+     */
+    protected $position;
+
+    /**
+     * @Gedmo\SortableGroup
+     * @ODM\Field(type="date")
+     */
+    protected $birthdate;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+    }
+
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    public function setPosition($position)
+    {
+        $this->position = $position;
+    }
+
+    public function getPosition()
+    {
+        return $this->position;
+    }
+
+    public function setBirthdate(\DateTime $birthdate)
+    {
+        $this->birthdate = $birthdate;
+    }
+
+    public function getBirthdate()
+    {
+        return $this->birthdate;
+    }
+}
