005e42669e506c0f4fac8e4bb7d8537532905ef1
Pavel Dubinin <geekdevs@gmail.com>
2014-11-16 16:21:18 +0300

    Allow storing NULL as blameable reference (for the case when user is not logged in)

--- lib/Gedmo/Blameable/BlameableListener.php
+++ lib/Gedmo/Blameable/BlameableListener.php
@@ -83,7 +83,7 @@ class BlameableListener extends TimestampableListener
         $newValue = $this->getUserValue($meta, $field);
 
         //if blame is reference, persist object
-        if ($meta->hasAssociation($field)) {
+        if ($newValue !== null && $meta->hasAssociation($field)) {
             $ea->getObjectManager()->persist($newValue);
         }
         $property->setValue($object, $newValue);
