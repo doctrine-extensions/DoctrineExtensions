0c4335d66596676e8b04b7b563108569bfa5d2ba
Steffen Roßkamp <steffen.rosskamp@gimmickmedia.de>
2014-12-18 18:49:31 +0100

    Add YAML inheritance mapping support for uploadable entities

--- lib/Gedmo/Uploadable/Mapping/Driver/Yaml.php
+++ lib/Gedmo/Uploadable/Mapping/Driver/Yaml.php
@@ -29,7 +29,14 @@ class Yaml extends File implements Driver
      */
     public function readExtendedMetadata($meta, array &$config)
     {
-        $mapping = $this->_getMapping($meta->name);
+        $mapping = array();
+        if (!$meta->isInheritanceTypeNone() && !$meta->isRootEntity()) {
+            $parents = array_reverse($meta->parentClasses);
+            foreach ($parents as $parent) {
+                $mapping = array_merge_recursive($mapping, $this->_getMapping($parent));
+            }
+        }
+        $mapping = array_merge_recursive($mapping, $this->_getMapping($meta->name));
 
         if (isset($mapping['gedmo'])) {
             $classMapping = $mapping['gedmo'];
