60a8bda651a63e110de6bc0bcb0baf9e7690c051
gedi <gediminas.morkevicius@gmail.com>
2015-07-30 23:25:14 +0300

    bump version

--- lib/Gedmo/DoctrineExtensions.php
+++ lib/Gedmo/DoctrineExtensions.php
@@ -23,7 +23,7 @@ final class DoctrineExtensions
     /**
      * Current version of extensions
      */
-    const VERSION = 'v2.4.2';
+    const VERSION = 'v2.4.4';
 
     /**
      * Hooks all extensions metadata mapping drivers
