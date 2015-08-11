12da6e0c6ffbc7702d9f83ff12f3649a0bc5b600
gedi <gediminas.morkevicius@gmail.com>
2015-08-11 19:33:57 +0300

    bump version for maintenance release

--- example/em.php
+++ example/em.php
@@ -9,7 +9,7 @@ $connection = array(
     'host' => '127.0.0.1',
     'port' => 3306,
     'user' => 'root',
-    'password' => 'nimda',
+    'password' => null,
     'dbname' => 'test',
     'driver' => 'pdo_mysql',
 );
--- lib/Gedmo/DoctrineExtensions.php
+++ lib/Gedmo/DoctrineExtensions.php
@@ -23,7 +23,7 @@ final class DoctrineExtensions
     /**
      * Current version of extensions
      */
-    const VERSION = 'v2.4.4';
+    const VERSION = 'v2.4.5';
 
     /**
      * Hooks all extensions metadata mapping drivers
