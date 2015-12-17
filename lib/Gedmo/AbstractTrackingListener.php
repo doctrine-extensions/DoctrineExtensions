8ca99b2aa9c7a6b4aed2d204414edae1adebd833
Adrien Brault <monsti@gmail.com>
2015-12-17 03:58:04 +0100

    Fix cyclical persists

--- lib/Gedmo/AbstractTrackingListener.php
+++ lib/Gedmo/AbstractTrackingListener.php
@@ -202,7 +202,7 @@ abstract class AbstractTrackingListener extends MappedEventSubscriber
         $newValue = $this->getFieldValue($meta, $field, $eventAdapter);
 
         // if field value is reference, persist object
-        if ($meta->hasAssociation($field) && is_object($newValue)) {
+        if ($meta->hasAssociation($field) && is_object($newValue) && !$eventAdapter->getObjectManager()->contains($newValue)) {
             $uow = $eventAdapter->getObjectManager()->getUnitOfWork();
 
             // Check to persist only when the entity isn't already managed, persists always for MongoDB
