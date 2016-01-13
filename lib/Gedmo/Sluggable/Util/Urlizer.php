881f4006b80ba5cc80537b1c3571dc71ff470dc0
Philip Iezzi <github@onlime.ch>
2016-01-13 08:58:57 +0100

    added support for sluggable umlauts/accents Fixes #1065

--- lib/Gedmo/Sluggable/Util/Urlizer.php
+++ lib/Gedmo/Sluggable/Util/Urlizer.php
@@ -9,4 +9,20 @@ use Behat\Transliterator\Transliterator;
  */
 class Urlizer extends Transliterator
 {
+    /**
+     * Generates a slug of the text after transliterating the UTF-8 string to ASCII.
+     *
+     * Unaccent umlauts/accents prior to transliteration.
+     * Uses transliteration tables to convert any kind of utf8 character.
+     *
+     * @param string $text
+     * @param string $separator
+     *
+     * @return string $text
+     */
+    public static function transliterate($text, $separator = '-')
+    {
+        $text = parent::unaccent($text);
+        return parent::transliterate($text, $separator);
+    }
 }
