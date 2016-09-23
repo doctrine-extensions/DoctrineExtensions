7d63a0b9d476a36e6ea6829003113f2e246f7c4b
David Heidrich <me@bowlingx.com>
2016-09-23 11:30:46 +0200

    fixed adding a slash at the beginning when path is NULL

--- lib/Gedmo/Tree/Strategy/AbstractMaterializedPath.php
+++ lib/Gedmo/Tree/Strategy/AbstractMaterializedPath.php
@@ -283,7 +283,7 @@ abstract class AbstractMaterializedPath implements Strategy
 
             $parentPath = $pathProp->getValue($parent);
             // if parent path not ends with separator
-            if ($parentPath[strlen($parentPath) - 1] !== $config['path_separator']) {
+            if ($parentPath && $parentPath[strlen($parentPath) - 1] !== $config['path_separator']) {
                 // add separator
                 $path = $pathProp->getValue($parent).$config['path_separator'].$path;
             } else {
