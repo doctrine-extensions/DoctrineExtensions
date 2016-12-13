94786b05ac588fa95eda3314be46268818062f2f
mcorteel <mcorteel@gmail.com>
2016-12-13 21:49:43 +0100

    Use mb_strlen to get string length to avoid unicode characters problems

--- lib/Gedmo/Sluggable/Mapping/Event/Adapter/ORM.php
+++ lib/Gedmo/Sluggable/Mapping/Event/Adapter/ORM.php
@@ -82,7 +82,7 @@ class ORM extends BaseAdapterORM implements SluggableAdapter
         $qb->update($config['useObjectClass'], 'rec')
             ->set('rec.'.$config['slug'], $qb->expr()->concat(
                 $qb->expr()->literal($replacement),
-                $qb->expr()->substring('rec.'.$config['slug'], strlen($target))
+                $qb->expr()->substring('rec.'.$config['slug'], mb_strlen($target))
             ))
             ->where($qb->expr()->like(
                 'rec.'.$config['slug'],
@@ -105,7 +105,7 @@ class ORM extends BaseAdapterORM implements SluggableAdapter
         $qb->update($config['useObjectClass'], 'rec')
             ->set('rec.'.$config['slug'], $qb->expr()->concat(
                 $qb->expr()->literal($target),
-                $qb->expr()->substring('rec.'.$config['slug'], strlen($replacement)+1)
+                $qb->expr()->substring('rec.'.$config['slug'], mb_strlen($replacement)+1)
             ))
             ->where($qb->expr()->like('rec.'.$config['slug'], $qb->expr()->literal($replacement . '%')))
         ;
