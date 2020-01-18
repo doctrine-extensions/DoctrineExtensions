ab228dbe59e46ec8182eaa43a7b3bea4baa0a484
Aken Roberts <aken@cryode.com>
2020-01-18 01:07:44 -0600

    Upgrade PHPUnit to 7.5
    - PHPUnit 7+ includes fixes for the deprecated ReflectionType::__toString method, which was causing errors on PHP 7.4
    - Base test files were updated to remove pointless method overloads that didn’t match PHPUnit definitions
    - Unused `syntaxCheck` phpunit.xml configuration item removed

--- tests/Gedmo/Tool/BaseTestCaseMongoODM.php
+++ tests/Gedmo/Tool/BaseTestCaseMongoODM.php
@@ -42,18 +42,6 @@ abstract class BaseTestCaseMongoODM extends \PHPUnit\Framework\TestCase
     /**
      * {@inheritdoc}
      */
-    public function expectException($exception)
-    {
-        if (method_exists('PHPUnit\\Framework\\TestCase', 'setExpectedException')) {
-            return parent::setExpectedException($exception);
-        }
-
-        return parent::expectException($exception);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
     protected function tearDown()
     {
         if ($this->dm) {
