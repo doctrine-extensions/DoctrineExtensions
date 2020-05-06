43283a76c48ccb6f0ed5ee54b09edc88072ca6f2
Florian Parreno <f.parreno@lexik.fr>
2020-05-06 10:09:46 +0200

    Remove interpreted annotations from phpdoc

--- lib/Gedmo/Mapping/Annotation/SlugHandler.php
+++ lib/Gedmo/Mapping/Annotation/SlugHandler.php
@@ -7,18 +7,6 @@ use Doctrine\Common\Annotations\Annotation;
 /**
  * SlugHandler annotation for Sluggable behavioral extension
  *
- * @Gedmo\Slug(handlers={
- *      @Gedmo\SlugHandler(class="Some\Class", options={
- *          @Gedmo\SlugHandlerOption(name="relation", value="parent"),
- *          @Gedmo\SlugHandlerOption(name="separator", value="/")
- *      }),
- *      @Gedmo\SlugHandler(class="Some\Class", options={
- *          @Gedmo\SlugHandlerOption(name="option", value="val"),
- *          ...
- *      }),
- *      ...
- * }, separator="-", updatable=false)
- *
  * @Annotation
  *
  * @author Gediminas Morkevicius <gediminas.morkevicius@gmail.com>
