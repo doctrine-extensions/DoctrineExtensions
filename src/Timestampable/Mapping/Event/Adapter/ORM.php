21a24cbc50c6d9d6f7da29ac8fb02b6a1939330b
Vytautas Stankus <svycka@gmail.com>
2020-08-24 10:52:50 +0300

    Remove zenddate date format support

--- src/Timestampable/Mapping/Event/Adapter/ORM.php
+++ src/Timestampable/Mapping/Event/Adapter/ORM.php
@@ -23,9 +23,6 @@ final class ORM extends BaseAdapterORM implements TimestampableAdapter
         if (isset($mapping['type']) && 'integer' === $mapping['type']) {
             return time();
         }
-        if (isset($mapping['type']) && 'zenddate' == $mapping['type']) {
-            return new \Zend_Date();
-        }
         if (isset($mapping['type']) && in_array($mapping['type'], ['date_immutable', 'time_immutable', 'datetime_immutable', 'datetimetz_immutable'], true)) {
             return new \DateTimeImmutable();
         }
