cce899dcf856a7f743092fed5a9db509ae5848d7
Aken Roberts <aken@cryode.com>
2021-03-27 16:16:14 -0500

    Add hacky fix for Doctrine Bundle 2.3

--- src/Mapping/ExtensionMetadataFactory.php
+++ src/Mapping/ExtensionMetadataFactory.php
@@ -2,6 +2,7 @@
 
 namespace Gedmo\Mapping;
 
+use Doctrine\Bundle\DoctrineBundle\Mapping\MappingDriver as DoctrineBundleMappingDriver;
 use Doctrine\Common\Cache\Cache;
 use Doctrine\Persistence\Mapping\Driver\DefaultFileLocator;
 use Doctrine\Persistence\Mapping\Driver\MappingDriver;
@@ -137,6 +138,12 @@ class ExtensionMetadataFactory
      */
     protected function getDriver($omDriver)
     {
+        if ($omDriver instanceof DoctrineBundleMappingDriver) {
+            $omDriver = (new \ReflectionClass($omDriver))
+                ->getProperty('driver')
+                ->getValue($omDriver);
+        }
+
         $driver = null;
         $className = get_class($omDriver);
         $driverName = substr($className, strrpos($className, '\\') + 1);
