02d3d540fdccb7b8457702e4dfb7ae42d8cd996f
Fran Moreno <franmomu@gmail.com>
2021-10-06 18:35:23 +0200

    Enfore rules with php-cs-fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -5,6 +5,9 @@ $finder = PhpCsFixer\Finder::create()
         __DIR__ . '/example',
         __DIR__ . '/src',
         __DIR__ . '/tests',
+    ])
+    ->exclude([
+        __DIR__ . '/tests/temp',
     ]);
 
 $config = new PhpCsFixer\Config();
@@ -13,8 +16,12 @@ return $config
         '@PSR2' => true,
         '@Symfony' => true,
         'array_syntax' => ['syntax' => 'short'],
+        'is_null' => false,
+        'list_syntax' => true,
+        'modernize_types_casting' => true,
         'ordered_imports' => ['sort_algorithm' => 'alpha'],
         'phpdoc_summary' => false,
         'phpdoc_to_comment' => false,
+        'ternary_to_null_coalescing' => true,
     ])
     ->setFinder($finder);
