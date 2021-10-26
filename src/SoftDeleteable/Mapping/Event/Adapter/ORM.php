3d45c003f612866691288c97bceff64e78bb8daa
Oleguer <oleguer.ml89@gmail.com>
2021-10-26 06:57:08 +0200

    Convert DateTime to PHP value using Doctrine converter. It allows using Timestampable and SoftDeleteable with other DateTime objects like Carbon. Also, code review suggestions from https://github.com/doctrine-extensions/DoctrineExtensions/pull/2248/commits/081925aaa9ce6ba7b94e179bb1412b498a256fc2 applied.

--- src/SoftDeleteable/Mapping/Event/Adapter/ORM.php
+++ src/SoftDeleteable/Mapping/Event/Adapter/ORM.php
@@ -2,6 +2,8 @@
 
 namespace Gedmo\SoftDeleteable\Mapping\Event\Adapter;
 
+use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Types\Types;
 use Gedmo\Mapping\Event\Adapter\ORM as BaseAdapterORM;
 use Gedmo\SoftDeleteable\Mapping\Event\SoftDeleteableAdapter;
 
@@ -20,14 +22,29 @@ final class ORM extends BaseAdapterORM implements SoftDeleteableAdapter
     public function getDateValue($meta, $field)
     {
         $mapping = $meta->getFieldMapping($field);
+        $converter = Type::getType($mapping['type'] ?? Types::DATETIME_MUTABLE);
+        $platform = $this->getObjectManager()->getConnection()->getDriver()->getDatabasePlatform();
+
+        return $converter->convertToPHPValue($this->getRawDateValue($mapping), $platform);
+    }
+
+    /**
+     * Generates current timestamp for the specified mapping
+     *
+     * @param array<string, mixed> $mapping
+     *
+     * @return \DateTimeInterface|int
+     */
+    private function getRawDateValue(array $mapping)
+    {
         if (isset($mapping['type']) && 'integer' === $mapping['type']) {
             return time();
         }
         if (isset($mapping['type']) && in_array($mapping['type'], ['date_immutable', 'time_immutable', 'datetime_immutable', 'datetimetz_immutable'], true)) {
             return new \DateTimeImmutable();
         }
-
         return \DateTime::createFromFormat('U.u', number_format(microtime(true), 6, '.', ''))
             ->setTimeZone(new \DateTimeZone(date_default_timezone_get()));
     }
+    
 }
