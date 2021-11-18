b19d1f7b51605ec38ba3f9fe1ad720923af5c142
Kevin Mian Kraiker <kevin.mian@gmail.com>
2021-11-18 13:02:41 -0300

    Update tests/Gedmo/Timestampable/Fixture/WithoutInterface.php

--- tests/Gedmo/Timestampable/Fixture/WithoutInterface.php
+++ tests/Gedmo/Timestampable/Fixture/WithoutInterface.php
@@ -2,6 +2,7 @@
 
 namespace Gedmo\Tests\Timestampable\Fixture;
 
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
 
@@ -18,13 +19,13 @@ class WithoutInterface
      */
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column(type: 'integer')]
+    #[ORM\Column(type: Types::INTEGER)]
     private $id;
 
     /**
      * @ORM\Column(type="string", length=128)
      */
-    #[ORM\Column(type: 'string', length: 128)]
+    #[ORM\Column(type: Types::STRING, length: 128)]
     private $title;
 
     /**
@@ -32,14 +33,14 @@ class WithoutInterface
      * @ORM\Column(type="date")
      */
     #[Gedmo\Timestampable(on: 'create')]
-    #[ORM\Column(type: 'date')]
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
     private $created;
 
     /**
      * @ORM\Column(type="datetime")
      * @Gedmo\Timestampable(on="update")
      */
-    #[ORM\Column(type: 'datetime')]
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
     #[Gedmo\Timestampable(on: 'update')]
     private $updated;
 
