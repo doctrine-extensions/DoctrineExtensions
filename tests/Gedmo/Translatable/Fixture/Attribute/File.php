6d14f2c48f6d40634e713b75e7ce99ae493de2d7
Fran Moreno <franmomu@gmail.com>
2021-11-18 13:54:30 +0100

    Add a test to show bug mixing attributes and annotations

---
+++ tests/Gedmo/Translatable/Fixture/Attribute/File.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Gedmo\Tests\Translatable\Fixture\Attribute;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+#[ORM\Entity]
+class File
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: Types::INTEGER)]
+    private $id;
+
+    /**
+     * @Gedmo\Translatable
+     */
+    #[ORM\Column(name: 'title', type: Types::STRING, length: 128)]
+    private $title;
+
+    /**
+     * @Gedmo\Locale
+     */
+    public $locale;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+}
