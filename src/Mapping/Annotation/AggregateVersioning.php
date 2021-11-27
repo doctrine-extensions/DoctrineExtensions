7f1836902ce4af426de1e8d1e3f59e2758ccd095
vorozcovmaksim <myks1992@mail.ru>
2021-11-27 18:31:23 +0300

    Fix PHPStan error

--- src/Mapping/Annotation/AggregateVersioning.php
+++ src/Mapping/Annotation/AggregateVersioning.php
@@ -9,13 +9,30 @@ use Doctrine\Common\Annotations\Annotation;
 
 /**
  * @Annotation
+ * @NamedArgumentConstructor
  * @Target("CLASS")
  *
  * @author Maksim Vorozhtsov <myks1992@mail.ru>
  */
 #[Attribute(Attribute::TARGET_CLASS)]
-final class AggregateVersioning extends Annotation
+final class AggregateVersioning implements \Gedmo\Mapping\Annotation\Annotation
 {
     /** @var string */
-    public $aggregateRootMethod;
+    public $aggregateRootMethod = 'getRoot';
+
+    /**
+     * @param array|string $data data array managed by the Doctrine Annotations library or the path
+     */
+    public function __construct(
+        $data = [],
+        string $aggregateRootMethod = 'getRoot'
+    ) {
+        if (\is_string($data)) {
+            $data = ['aggregateRootMethod' => $data];
+        } elseif (!\is_array($data)) {
+            throw new \TypeError(sprintf('"%s": Argument $data is expected to be a string or array, got "%s".', __METHOD__, get_debug_type($data)));
+        }
+
+        $this->aggregateRootMethod = $data['aggregateRootMethod'] ?? $aggregateRootMethod;
+    }
 }
