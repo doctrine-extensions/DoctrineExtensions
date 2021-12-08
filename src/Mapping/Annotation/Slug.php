dd186c289c5d9fa1fedd1339539e8904b65a9a1a
Tac Tacelosky <tacman@gmail.com>
2021-12-08 08:13:18 -0500

    constructor for Slug to use php8 attributes

--- src/Mapping/Annotation/Slug.php
+++ src/Mapping/Annotation/Slug.php
@@ -9,7 +9,9 @@
 
 namespace Gedmo\Mapping\Annotation;
 
+use Attribute;
 use Doctrine\Common\Annotations\Annotation;
+use Gedmo\Mapping\Annotation\Annotation as GedmoAnnotation;
 
 /**
  * Slug annotation for Sluggable behavioral extension
@@ -19,7 +21,8 @@ use Doctrine\Common\Annotations\Annotation;
  *
  * @author Gediminas Morkevicius <gediminas.morkevicius@gmail.com>
  */
-final class Slug extends Annotation
+#[Attribute(Attribute::TARGET_PROPERTY)]
+final class Slug implements GedmoAnnotation
 {
     /** @var array<string> @Required */
     public $fields = [];
@@ -41,4 +44,38 @@ final class Slug extends Annotation
     public $handlers = [];
     /** @var string */
     public $dateFormat = 'Y-m-d-H:i';
+
+    /**
+     * @phpstan-param class-string|null $type
+     */
+    public function __construct(array $data = [],
+                                array $fields = [],
+                                bool $updatable = true,
+                                string $style = 'default', // or "camel"
+                                bool $unique = true,
+                                string $unique_base = '',
+                                string $separator = '-',
+                                string $prefix = '',
+                                string $suffix = '',
+                                iterable $handlers = [],
+                                string $dateFormat = 'Y-m-d-H:i'
+    ) {
+        if ([] !== $data) {
+            @trigger_error(sprintf(
+                'Passing an array as first argument to "%s()" is deprecated. Use named arguments instead.',
+                __METHOD__
+            ), E_USER_DEPRECATED);
+        }
+
+        $this->fields = $data['fields'] ?? $fields;
+        $this->updatable = $data['updatable'] ?? $updatable;
+        $this->style = $data['style'] ?? $style;
+        $this->unique = $data['unique'] ?? $unique;
+        $this->unique_base = $data['unique_base'] ?? $unique_base;
+        $this->separator = $data['separator'] ?? $separator;
+        $this->prefix = $data['prefix'] ?? $prefix;
+        $this->suffix = $data['suffix'] ?? $suffix;
+        $this->handlers = $data['handlers'] ?? $handlers;
+        $this->dateFormat = $data['dateFormat'] ?? $dateFormat;
+    }
 }
