2def628a9a8304c1e7f56847a0b21778a3a9ed61
Filippo Tessarotto <zoeslam@gmail.com>
2022-01-31 14:15:02 +0100

    Closure: mirror Doctrine cache key

--- src/Tree/Strategy/ORM/Closure.php
+++ src/Tree/Strategy/ORM/Closure.php
@@ -198,7 +198,12 @@ class Closure implements Strategy
             $cacheDriver = $cmf->getCacheDriver();
 
             if ($cacheDriver instanceof Cache) {
-                $cacheDriver->save($closureMetadata->getName().'$CLASSMETADATA', $closureMetadata);
+                // @see https://github.com/doctrine/persistence/pull/144
+                // @see \Doctrine\Persistence\Mapping\AbstractClassMetadataFactory::getCacheKey()
+                $cacheDriver->save(
+                    str_replace('\\', '__', $closureMetadata->getName()).'__CLASSMETADATA__',
+                    $closureMetadata
+                );
             }
         }
     }
