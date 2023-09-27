e3bd0837cb113e1c065a7277696e46d6515b7ac6
m-idler <marcel.idler@mayflower.de>
2023-09-27 14:11:32 +0200

    [Translatable][Translator] unify alias usage of doctrine `ORM` mapping in entities

--- src/Translatable/Entity/Translation.php
+++ src/Translatable/Entity/Translation.php
@@ -9,40 +9,35 @@
 
 namespace Gedmo\Translatable\Entity;
 
-use Doctrine\ORM\Mapping\Entity;
-use Doctrine\ORM\Mapping\Index;
-use Doctrine\ORM\Mapping\Table;
-use Doctrine\ORM\Mapping\UniqueConstraint;
+use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Translatable\Entity\MappedSuperclass\AbstractTranslation;
 use Gedmo\Translatable\Entity\Repository\TranslationRepository;
 
 /**
  * Gedmo\Translatable\Entity\Translation
  *
- * @Table(
+ * @ORM\Table(
  *     name="ext_translations",
  *     options={"row_format": "DYNAMIC"},
  *     indexes={
- *
- *         @Index(name="translations_lookup_idx", columns={
+ *         @ORM\Index(name="translations_lookup_idx", columns={
  *             "locale", "object_class", "foreign_key"
  *         }),
- *         @Index(name="general_translations_lookup_idx", columns={
+ *         @ORM\Index(name="general_translations_lookup_idx", columns={
  *             "object_class", "foreign_key"
  *         })
  *     },
- *     uniqueConstraints={@UniqueConstraint(name="lookup_unique_idx", columns={
+ *     uniqueConstraints={@ORM\UniqueConstraint(name="lookup_unique_idx", columns={
  *         "locale", "object_class", "field", "foreign_key"
  *     })}
  * )
- *
- * @Entity(repositoryClass="Gedmo\Translatable\Entity\Repository\TranslationRepository")
+ * @ORM\Entity(repositoryClass="Gedmo\Translatable\Entity\Repository\TranslationRepository")
  */
-#[Entity(repositoryClass: TranslationRepository::class)]
-#[Table(name: 'ext_translations', options: ['row_format' => 'DYNAMIC'])]
-#[Index(name: 'translations_lookup_idx', columns: ['locale', 'object_class', 'foreign_key'])]
-#[Index(name: 'general_translations_lookup_idx', columns: ['object_class', 'foreign_key'])]
-#[UniqueConstraint(name: 'lookup_unique_idx', columns: ['locale', 'object_class', 'field', 'foreign_key'])]
+#[ORM\Entity(repositoryClass: TranslationRepository::class)]
+#[ORM\Table(name: 'ext_translations', options: ['row_format' => 'DYNAMIC'])]
+#[ORM\Index(name: 'translations_lookup_idx', columns: ['locale', 'object_class', 'foreign_key'])]
+#[ORM\Index(name: 'general_translations_lookup_idx', columns: ['object_class', 'foreign_key'])]
+#[ORM\UniqueConstraint(name: 'lookup_unique_idx', columns: ['locale', 'object_class', 'field', 'foreign_key'])]
 class Translation extends AbstractTranslation
 {
     /*
