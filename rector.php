0199d49839db65841694e2a2b6a60189255f6fcc
Oleg Andreyev <oleg@andreyev.lv>
2024-03-11 20:32:09 +0200

    phpcs

--- rector.php
+++ rector.php
@@ -12,7 +12,6 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Doctrine\Set\DoctrineSetList;
 use Rector\Set\ValueObject\LevelSetList;
-use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromAssignsRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
