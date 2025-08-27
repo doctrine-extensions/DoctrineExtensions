d6b976047413d6dc6c27149b5be67a218de4f939
Ruben Kluft <ruben@123inkt.nl>
2025-08-27 15:28:03 +0200

    Add NonDeletedValue test for Mappings

---
+++ tests/Gedmo/Mapping/Fixture/SoftDeleteableNonDeletedColumnValue.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Doctrine Behavioral Extensions package.
+ * (c) Gediminas Morkevicius <gediminas.morkevicius@gmail.com> http://www.gediminasm.org
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Gedmo\Tests\Mapping\Fixture;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * @ORM\Entity
+ *
+ * @Gedmo\SoftDeleteable(fieldName="deletedAt", hardDelete: false, nonDeletedColumnValue: '1970-01-01 00:00:00')
+ */
+#[ORM\Entity]
+#[Gedmo\SoftDeleteable(fieldName: 'deletedAt', hardDelete: false, nonDeletedColumnValue: '1970-01-01 00:00:00')]
+class SoftDeleteableNonDeletedColumnValue
+{
+    /**
+     * @var int|null
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: Types::INTEGER)]
+    private $id;
+
+    private ?string $title = null;
+
+    private ?string $code = null;
+
+    /**
+     * @var string|null
+     */
+    private $slug;
+
+    /**
+     * @var \DateTime|null
+     *
+     * @ORM\Column(name="deleted_at", type="datetime", nullable=true)
+     */
+    #[ORM\Column(name: 'deleted_at', type: Types::DATETIME_MUTABLE, nullable: true)]
+    private $deletedAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setTitle(?string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setCode(?string $code): void
+    {
+        $this->code = $code;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+}
---
+++ tests/Gedmo/Mapping/SoftDeleteableMappingNonDeletedValueTest.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Doctrine Behavioral Extensions package.
+ * (c) Gediminas Morkevicius <gediminas.morkevicius@gmail.com> http://www.gediminasm.org
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Gedmo\Tests\Mapping;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
+use Doctrine\ORM\Mapping\Driver\YamlDriver;
+use Gedmo\Mapping\ExtensionMetadataFactory;
+use Gedmo\SoftDeleteable\SoftDeleteableListener;
+use Gedmo\Tests\Mapping\Fixture\SoftDeleteableNonDeletedColumnValue as AnnotatedSoftDeleteable;
+use Gedmo\Tests\Mapping\Fixture\Xml\SoftDeleteableNonDeletedColumnValue as XmlSoftDeleteable;
+use Gedmo\Tests\Mapping\Fixture\Yaml\SoftDeleteableNonDeletedColumnValue as YamlSoftDeleteable;
+
+/**
+ * These are mapping tests for SoftDeleteable extension
+ *
+ * @author Gustavo Falco <comfortablynumb84@gmail.com>
+ * @author Gediminas Morkevicius <gediminas.morkevicius@gmail.com>
+ */
+final class SoftDeleteableMappingNonDeletedValueTest extends ORMMappingTestCase
+{
+    private EntityManager $em;
+
+    protected function setUp(): void
+    {
+        ORMMappingTestCase::setUp();
+
+        $listener = new SoftDeleteableListener();
+        $listener->setCacheItemPool($this->cache);
+
+        $this->em = $this->getBasicEntityManager();
+        $this->em->getEventManager()->addEventSubscriber($listener);
+    }
+
+    /**
+     * @return \Generator<string, array{class-string}>
+     */
+    public static function dataSoftDeleteableObject(): \Generator
+    {
+        yield 'Model with XML mapping' => [XmlSoftDeleteable::class];
+
+        if (PHP_VERSION_ID >= 80000) {
+            yield 'Model with attributes' => [AnnotatedSoftDeleteable::class];
+        } elseif (class_exists(AnnotationDriver::class)) {
+            yield 'Model with annotations' => [AnnotatedSoftDeleteable::class];
+        }
+
+        if (class_exists(YamlDriver::class)) {
+            yield 'Model with YAML mapping' => [YamlSoftDeleteable::class];
+        }
+    }
+
+    /**
+     * @param class-string $className
+     *
+     * @dataProvider dataSoftDeleteableObject
+     */
+    public function testSoftDeleteableMapping(string $className): void
+    {
+        // Force metadata class loading.
+        $this->em->getClassMetadata($className);
+        $cacheId = ExtensionMetadataFactory::getCacheId($className, 'Gedmo\SoftDeleteable');
+        $config = $this->cache->getItem($cacheId)->get();
+
+        static::assertArrayHasKey('nonDeletedColumnValue', $config);
+        static::assertSame('1970-01-01 00:00:00', $config['nonDeletedColumnValue']);
+    }
+}
