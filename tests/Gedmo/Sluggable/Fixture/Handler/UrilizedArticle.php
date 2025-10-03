c2ba2a8e6f3b01dd6cc9c9c952dd7f5c6258e1da
Michael Babker <michael@happydog.digital>
2025-10-03 10:07:29 -0400

    Add tests for entities using the urilize relative slug handler option

---
+++ tests/Gedmo/Sluggable/Fixture/Handler/UrilizedArticle.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Doctrine Behavioral Extensions package.
+ * (c) Gediminas Morkevicius <gediminas.morkevicius@gmail.com> http://www.gediminasm.org
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Gedmo\Tests\Sluggable\Fixture\Handler;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Gedmo\Sluggable\Handler\InversedRelativeSlugHandler;
+use Gedmo\Sluggable\Sluggable;
+
+/**
+ * @ORM\Entity
+ */
+#[ORM\Entity]
+class UrilizedArticle implements Sluggable
+{
+    /**
+     * @var int|null
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: Types::INTEGER)]
+    private $id;
+
+    /**
+     * @ORM\Column(name="title", type="string", length=64)
+     */
+    #[ORM\Column(name: 'title', type: Types::STRING, length: 64)]
+    private ?string $title = null;
+
+    /**
+     * @ORM\Column(name="code", type="string", length=16)
+     */
+    #[ORM\Column(name: 'code', type: Types::STRING, length: 16)]
+    private ?string $code = null;
+
+    /**
+     * @var string|null
+     *
+     * @Gedmo\Slug(handlers={
+     *     @Gedmo\SlugHandler(class="Gedmo\Sluggable\Handler\InversedRelativeSlugHandler", options={
+     *         @Gedmo\SlugHandlerOption(name="relationClass", value="Gedmo\Tests\Sluggable\Fixture\Handler\UrilizedArticleRelativeSlug"),
+     *         @Gedmo\SlugHandlerOption(name="mappedBy", value="article"),
+     *         @Gedmo\SlugHandlerOption(name="inverseSlugField", value="slug")
+     *     })
+     * }, separator="-", updatable=true, fields={"title", "code"})
+     *
+     * @ORM\Column(name="slug", type="string", length=64, unique=true)
+     */
+    #[Gedmo\Slug(separator: '-', updatable: true, fields: ['title', 'code'])]
+    #[Gedmo\SlugHandler(class: InversedRelativeSlugHandler::class, options: ['relationClass' => UrilizedArticleRelativeSlug::class, 'mappedBy' => 'article', 'inverseSlugField' => 'slug'])]
+    #[ORM\Column(name: 'slug', type: Types::STRING, length: 64, unique: true)]
+    private $slug;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setTitle(?string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setCode(?string $code): void
+    {
+        $this->code = $code;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+}
