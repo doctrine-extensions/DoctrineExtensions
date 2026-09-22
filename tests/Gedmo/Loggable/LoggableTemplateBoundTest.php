12a746c2e9eaa49b2dc344be38ccf35158052d8e
Francis Hilaire <Prometee@users.noreply.github.com>
2026-09-22 11:24:20 +0200

    PHP CS Fixer fix

--- tests/Gedmo/Loggable/LoggableTemplateBoundTest.php
+++ tests/Gedmo/Loggable/LoggableTemplateBoundTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 /*
  * This file is part of the Doctrine Behavioral Extensions package.
- * (c) Gediminas Morkevicius <gediminasm@gmail.com>
+ * (c) Gediminas Morkevicius <gediminas.morkevicius@gmail.com> http://www.gediminasm.org
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -39,7 +39,7 @@ final class LoggableTemplateBoundTest extends TestCase
     {
         $docComment = (new \ReflectionClass($className))->getDocComment();
 
-        self::assertIsString($docComment);
-        self::assertStringContainsString($template.' T of object', $docComment);
+        static::assertIsString($docComment);
+        static::assertStringContainsString($template.' T of object', $docComment);
     }
 }
