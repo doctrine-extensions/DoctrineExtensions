1fa3084fcf5f0629943aae25159244b7124d1389
Stefan Gehrig <stefan.gehrig.hn@googlemail.com>
2026-09-25 18:41:58 +0200

    run php-cs-fixer
    Signed-off-by: Stefan Gehrig <stefan.gehrig.hn@googlemail.com>

--- tests/Gedmo/Blameable/Fixture/Entity/CompanySmallInt.php
+++ tests/Gedmo/Blameable/Fixture/Entity/CompanySmallInt.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file is part of the Doctrine Behavioral Extensions package.
+ * (c) Gediminas Morkevicius <gediminas.morkevicius@gmail.com> http://www.gediminasm.org
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Gedmo\Tests\Blameable\Fixture\Entity;
 
 use Doctrine\DBAL\Types\Types;
@@ -34,6 +41,7 @@ class CompanySmallInt
      * @var int|null
      *
      * @Gedmo\Blameable(on="create")
+     *
      * @ORM\Column(name="creator", type="smallint")
      */
     #[ORM\Column(name: 'creator', type: Types::SMALLINT)]
